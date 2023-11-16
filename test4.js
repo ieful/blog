fcce30ebceedeb74ecffb3c268273ec7844d2f98
allen <xiaoyu_17@qq.com>
2023-11-17 07:42:31 +0800

    add algolia

--- test4.js
+++ test4.js
@@ -1,38 +1,10 @@
 
-// 调度器
-class Test {
-    constructor() {
-        this.taskArr = [];
-    }
-    push (task) {
-        this.taskArr.push(task);
-    }
-    doFetch () {
-        main(maxRequst, this.taskArr)
+
+function myFind(nums) {
+    let temp = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+
     }
 }
 
-let test = new Test()
-
-// 并发控制
-let task = [1,2,3,4,5,6]
-function main(maxRequst, taskArr) {
-    return new Promise((resolve, reject) => {
-        let curr = 0;
-        let result = [];
-        async function doReq() {
-            let i = curr;
-            result[i] = await fetch(taskArr[i]);
-            curr += 1;
-            if (curr === taskArr.length) {
-                resolve(result);
-            } else {
-                doReq();
-            }
-        }
-        // 触发
-        for (let i = 0; i < maxRequst; i++) {
-            doReq();
-        }
-    })
-}
+console.log(myFind([1,3,4,2,2]))
