38e5b4ad605152c64ca5b6e2cd96e3a02b3d7711
xiaoyu <gs1521adc@buaa.edu.cn>
2024-03-22 21:08:01 +0800

    feat: 纯Css实现一滴水

---
+++ docs/Components/WaterDrop/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+
+const WaterDrop = () => {
+    return (
+        <div className="zone">
+            <div id="water"></div>
+        </div>
+    )
+}
+
+export default WaterDrop;
