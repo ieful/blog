7a92bfc9a13e1872db0c1cd41640e6964408bd40
xiaoyu <gs1521adc@buaa.edu.cn>
2024-03-27 00:07:44 +0800

    feat: async和defer的脚本加载

---
+++ docs/Components/JsPhaseLegend/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JsPhaseLegend = () => {
+    return (
+        <img src='https://www.growingwiththeweb.com/images/2014/02/26/legend.svg' width={300}/>
+    )
+}
+
+export default JsPhaseLegend;
