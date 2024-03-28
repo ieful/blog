bd9c513484490990835b8c3c3bf4c4199104083c
xiaoyu <gs1521adc@buaa.edu.cn>
2024-03-28 22:20:33 +0800

    ALin

--- node/generateMdx.js
+++ node/generateMdx.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const sourceFolder = '/Users/wangxiaoyu/Allen/blog/static/audio/S.H.E';
-const targetFolder = '/Users/wangxiaoyu/Allen/blog/docs/Me/songs/S.H.E';
+const sourceFolder = '/Users/wangxiaoyu/Allen/blog/static/audio/ALin';
+const targetFolder = '/Users/wangxiaoyu/Allen/blog/docs/Me/songs/ALin';
 
 function createMdxFile(fileName) {
     const mdxContent = `---
@@ -11,7 +11,7 @@ tags: [Music]
 ---
 
 <audio controls>
-    <source src="/audio/S.H.E/${fileName}.mp3" type="audio/mpeg" />
+    <source src="/audio/ALin/${fileName}.mp3" type="audio/mpeg" />
     Your browser does not support the audio element.
 </audio>
 `;
