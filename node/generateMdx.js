2ada309a84442e9fef798fa52de7de5e2bf36aae
xiaoyu <gs1521adc@buaa.edu.cn>
2024-03-28 22:15:11 +0800

    JJ

--- node/generateMdx.js
+++ node/generateMdx.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const sourceFolder = '/Users/wangxiaoyu/Allen/blog/static/audio/G.E.M';
-const targetFolder = '/Users/wangxiaoyu/Allen/blog/docs/Me/songs/G.E.M';
+const sourceFolder = '/Users/wangxiaoyu/Allen/blog/static/audio/JJ';
+const targetFolder = '/Users/wangxiaoyu/Allen/blog/docs/Me/songs/JJ';
 
 function createMdxFile(fileName) {
     const mdxContent = `---
@@ -11,7 +11,7 @@ tags: [Music]
 ---
 
 <audio controls>
-    <source src="/audio/G.E.M/${fileName}.mp3" type="audio/mpeg" />
+    <source src="/audio/JJ/${fileName}.mp3" type="audio/mpeg" />
     Your browser does not support the audio element.
 </audio>
 `;
