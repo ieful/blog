a69e000010f6efa93037aae57d582c76231e0c60
xiaoyu <gs1521adc@buaa.edu.cn>
2024-03-31 21:04:21 +0800

    favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'xiaoyu.work',
   tagline: '⌨️ still hunting for job... 🖱',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/githubavator.png',
 
   // Set the production url of your site here
   url: 'https://blog.xiaoyu.work/',
