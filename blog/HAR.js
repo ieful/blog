718a32f1150ec22325b717ff8acbe628dd01de12
xiaoyu <gs1521adc@buaa.edu.cn>
2024-04-06 18:52:09 +0800

    feat: add flv

--- blog/HAR.js
+++ blog/HAR.js
@@ -1,14 +1,19 @@
 import React from "react";
-
+// import harflv from '/static/flv/har.flv';
+// import flvjs from 'flv.js';
 
 const HarDemo = () => {
+
+    // const flvurl = URL.createObjectURL(harflv);
+
+    // let flvPlayer = flvjs.createPlayer({
+    //     type: 'flv',
+    //     url: ''
+    // })
+
     return (
-        <>
-            <video controls width="800">
-                <source src="/video/HAR.mp4" type="video/mp4"/>
-            </video>
-        </>
+        <video controls width="700" src="/video/HAR.mp4" />
     )
 }
 
-export default HarDemo
+export default HarDemo;
--- blog/Write.js
+++ blog/Write.js
@@ -4,7 +4,7 @@ import React from "react";
 const Write = () => {
     return (
         <>
-            <video controls width="800">
+            <video controls width="700">
                 <source src="/video/write.mp4" type="video/mp4"/>
             </video>
         </>
