6afb9ae2be186eb350ce0e1de45edceb449a211e
wangxiaoyu <wangxiaoyu@360.cn>
2024-04-07 11:28:53 +0800

    blogSidebarCount: all

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,7 @@ const config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarCount: 'ALL', // 显示所有博客
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
