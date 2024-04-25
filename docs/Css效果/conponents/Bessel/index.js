0cec732c230f5000eb170cf84777324115e52881
wangxiaoyu <wangxiaoyu@360.cn>
2024-04-25 10:39:55 +0800

    加入购物车

---
+++ docs/Css效果/conponents/Bessel/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+
+
+
+const Bessel = () => {
+    return (
+        <div className='playground'>
+            <div className='redball'>
+                <div className='inner'></div>
+            </div>
+        </div>
+    )
+}
+
+export default Bessel;
