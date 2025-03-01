0f41a1554b19ce6688cc497a227ee296f4e86b26
XIAOYU <gs1521adc@buaa.edu.cn>
2025-03-01 18:42:17 +0800

    del hunting for job

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'xiaoyu.work',
-  tagline: '🙏 Hunting for a JOB...🥺',
+  // tagline: 'Hunting for a JOB',
   favicon: 'img/githubavator.png',
 
   // Set the production url of your site here
